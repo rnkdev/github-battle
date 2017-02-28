e7e84f054658f625b29931552a694f71975f92c6
rntesla <rnkdev@users.noreply.github.com>
2017-02-28 20:27:40 +0700

    connect github api using axios

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,9 +1,13 @@
 var React = require('react');
 
+function testPrint (object) {
+    return <pre>{JSON.stringify(object, null, ' ')}</pre>
+}
+
 function ConfirmBattle (props) {
     return props.isLoading === true
         ? <p> Loading! </p>
-        : <p> Confirm Battle </p>
+        : <p> Confirm Battle : {testPrint(props)} </p>
 }
 
 module.exports = ConfirmBattle;
--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ConfirmBattle = require('../components/ConfirmBattle');
+var githubHelpers = require('../utils/githubHelpers');
 
 var ConfirmBattleContainer = React.createClass({
     contextTypes: {
@@ -14,6 +15,14 @@ var ConfirmBattleContainer = React.createClass({
     componentDidMount: function () {
 	var query = this.props.location.query;
 	// To do: Hook github api
+	githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+	    .then(function (players) {
+		this.setState({
+		    isLoading: false,
+		    playersInfo: [players[0], players[1]]
+
+		})
+	    }.bind(this))
     },
     render: function() {
 	return (
